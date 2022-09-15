08b0034db5f6a2a6a7d30791510720824d94b4e9
PUCHACORE - PC <puchacore@gmail.com>
2022-09-15 13:09:03 -0500

    Commit para todos los archivos
    Por defecto ya se agregaron los documentos a la lista del seguimiento de cambios hecho por Git.

---
+++ README.m
@@ -0,0 +1,21 @@
+# programacionEstructurada
+
+# REFERENCIA BIBLIOGRÁFICA
+
+
+# Algoritmos y diagramas de flujo:
+# https://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=326:pseudocodigo-y-diagrama-de-flujo-ecuacion-cuadratica-2o-grado-si-entonces-sino-if-then-else-cu00143a&catid=28&Itemid=59 
+
+# https://es.slideshare.net/fercastilloipn/anexo-1-diagramas-de-flujo
+# https://www.areatecnologia.com/informatica/ejemplos-de-diagramas-de-flujo.html
+# https://www.studocu.com/es/document/universidad-de-valladolid/programacion/anexo-1-diagramas-de-flujo-2/6077233
+# https://www.uv.mx/personal/aherrera/files/2020/05/DIAGRAMAS-DE-FLUJO.pdf
+# https://studylib.es/doc/8220426/diagramas-de-flujo
+
+# https://issuu.com/josuegarcia79/docs/fundamentos_de_programacion_con_scr/27
+
+# https://combomix.net/wp-content/uploads/2017/03/Fundamentos-de-programaci%C3%B3n-4ta-Edici%C3%B3n-Luis-Joyanes-Aguilar-2.pdf
+# https://github.com/davidmp/AFundamentosP/blob/master/Fundamentos-de-programaci%C3%B3n-4ta-Edici%C3%B3n-Luis-Joyanes-Aguilar-2.pdf
+# https://raw.githubusercontent.com/davidmp/AFundamentosP/master/Fundamentos-de-programaci%C3%B3n-4ta-Edici%C3%B3n-Luis-Joyanes-Aguilar-2.pdf
+
+# *************
